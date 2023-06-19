4720ca3c1e808e10e079d7ddfcaa4fb7e6b25345
fahmi.ahmad.tif <fahmi.ahmad.tif21@polban.ac.id>
2023-06-19 16:52:34 +0700

    ABOUT: page initialization (V1.0)

---
+++ pages/1_About.py
@@ -0,0 +1,19 @@
+# Package / Library
+import streamlit as st
+
+# MAIN PAGE
+def main():
+    # Title
+    st.set_page_config(
+        page_title="About",
+        page_icon="🧊",
+        layout="centered",
+        initial_sidebar_state="auto",
+    )
+
+# FUNCTIONS
+# 
+
+# MAIN CONFIG
+if __name__ == "__main__":
+    main()
---
+++ pages/2_Color_Space.py
@@ -0,0 +1,12 @@
+# Package / Library
+import streamlit as st
+
+# MAIN PAGE
+def main():
+    print("About Page")
+
+# FUNCTIONS
+
+# MAIN CONFIG
+if __name__ == "__main__":
+    main()
